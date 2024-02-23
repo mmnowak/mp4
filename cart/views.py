03795d71f241d0250da1e5671dbf12d0e0284a4e
Martyna Nowak <m.m.nowak187@gmail.com>
2024-02-23 20:32:20 +0000

    add toasts css, cart preview and additional messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -23,6 +23,7 @@ def add_to_cart(request, product_id):
 
     if product_id in list(cart.keys()):
         cart[product_id] += quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {cart[product_id]}')
     else:
         cart[product_id] = quantity
         messages.success(request, f'Added {product.product_name} to your cart')
@@ -34,27 +35,33 @@ def add_to_cart(request, product_id):
 
 def adjust_cart(request, product_id):
     """ Adjust quantity of the specified product in the shopping cart """
-
+    product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[product_id] = quantity
+        messages.success(request, f'Updated {product.product_name} quantity to {cart[product_id]}')
     else:
         cart.pop(product_id)
+        messages.success(request, f'Removed {product.product_name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
     
 def remove_from_cart(request, product_id):
     """ Removes the specified product from the shopping cart """
+    product = get_object_or_404(Product, pk=product_id)
+
     try:
         cart = request.session.get('cart', {})
 
         cart.pop(product_id)
+        messages.success(request, f'Removed {product.product_name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
-    except Exception as e:
+    except Exception as e:        
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
