45a8d19d35e05380f1d5ebd637a0c4581a506da5
Martyna Nowak <m.m.nowak187@gmail.com>
2024-02-25 14:27:11 +0000

    customize product admin forms

--- products/forms.py
+++ products/forms.py
@@ -13,8 +13,6 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        product_types = [(c.id, c.get_product_type()) for c in categories]
 
-        self.fields['category'].choices = product_types
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
