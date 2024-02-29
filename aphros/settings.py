a9f18900c4fdae4289bd562a529e7108ca418075
Martyna Nowak <m.m.nowak187@gmail.com>
2024-02-29 21:56:35 +0000

    secret key

--- aphros/settings.py
+++ aphros/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY, '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '9u_7_k3yzy5=ifcy9_(01la0lw6yt)v3%gi@-a0@x1pj462ee-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
