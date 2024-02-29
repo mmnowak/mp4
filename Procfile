2a597586c8ca51c553be737709c3f8a2ff407899
Martyna Nowak <m.m.nowak187@gmail.com>
2024-02-29 00:07:07 +0000

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn aphros.wsgi:application
--- aphros/settings.py
+++ aphros/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%xsn%+(jk(a(+y%dv6@rb3(l1mw^7c)^uoloeev%oz5qetbzm!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mmnowak-mp4-v2j8k5chl14.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['aphros-4bc91bf82566.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,14 +117,19 @@ WSGI_APPLICATION = 'aphros.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
