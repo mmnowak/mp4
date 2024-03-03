42981e594404bfe6d81b31a3066e41c8149ccf6a
Martyna Nowak <m.m.nowak187@gmail.com>
2024-03-03 20:44:18 +0000

    add favicon

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    UserProfile Model Admin
+    """
+
+    fields = (
+        'user', 'default_street_address1', 'default_street_address2',
+        'default_town_or_city', 'default_postcode',
+        'default_phone_number',)
+
+    list_display = (
+        'user', 'default_street_address1', 'default_street_address2',
+        'default_town_or_city', 'default_postcode',
+        'default_phone_number',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
